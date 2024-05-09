b068b494801eb53d07b20d27c7832b3c086a0d98
Nataliaada <106996389+Nataliaada@users.noreply.github.com>
2024-05-09 21:38:18 +0300

    Add files via upload

--- HW6 cold/unsplash_d/unsplash_d/pipelines.py
+++ HW6 cold/unsplash_d/unsplash_d/pipelines.py
@@ -27,3 +27,14 @@ class UnsplashCsvPipeline:
         self.writer.writerow([image_url, local_path, image_name, category])
 
         return item
+
+
+from scrapy.pipelines.images import ImagesPipeline
+
+class AnimalsImagesPipeline(ImagesPipeline): 
+    def get_media_requests(self, item, info):
+        for image_url in item.get('image_urls', []):
+            yield scrapy.Request(image_url)
+
+def file_path(self, request, response=None, info=None, *,item=None ):
+              return f"{item['category']}_{item['image_url']}.jpg"
--- HW6 cold/unsplash_d/unsplash_d/spiders/unsplash.py
+++ HW6 cold/unsplash_d/unsplash_d/spiders/unsplash.py
@@ -4,41 +4,36 @@ from scrapy.spiders import CrawlSpider, Rule
 
 import re
 import os
+import scrapy
+import os
 
 class UnsplashImageItem(scrapy.Item):
-    image_url = scrapy.Field()
-    image_name = scrapy.Field()
-    category = scrapy.Field()
-
-# //*[@class="oaSYM ZR5jm"] категория
-# //*[@class="WxXog"] фото
-# //*[@class="WxXog"]/img/@srcset LinkExtractor
+ image_url = scrapy.Field()
+ image_name = scrapy.Field()
+ category = scrapy.Field()
 
 class UnsplashSpider(scrapy.Spider):
-    name = 'unsplash'
-    allowed_domains = ['unsplash.com']
-    start_urls = ['https://unsplash.com/']
+ name = 'unsplash'
+ allowed_domains = ['unsplash.com']
+ start_urls = ['https://unsplash.com/']
 
-    def parse(self, response):
-        for category in response.xpath('//*[@class="oaSYM ZR5jm"]/@href').extract():
-            yield scrapy.Request(response.urljoin(category), self.parse_category)
+ def parse(self, response):
+    for category in response.xpath('//*[@class="oaSYM ZR5jm"]/@href').extract():
+        yield scrapy.Request(response.urljoin(category), callback=self.parse_category)
 
-    def parse_category(self, response):
-        # Извлекаем ссылки на изображения и их разрешения из srcset
-        for image_url, image_name in zip(response.xpath('//*[@class="WxXog"]/img/@srcset').extract(), response.xpath('//*[@class="WxXog"]/img/@alt').extract()):
-            # Выводим элемент изображения
-            yield UnsplashImageItem(category=response.url.split('/')[-1], image_name=image_name, image_url=image_url)
+ def parse_category(self, response):
+    category = response.url.split('/')[-1]
+    image_urls = response.xpath('//*[@class="WxXog"]/img/@src').extract()
+    image_names = response.xpath('//*[@class="WxXog"]/img/@alt').extract()
+    selector = scrapy.Selector(response=response, type='html')
+    for image_url, image_name in zip(image_urls, image_names):
+        yield UnsplashImageItem(category=category, image_name=image_name, image_url=image_url)
+        yield scrapy.Request(response.urljoin(image_url), callback=self.save_image, meta={'category': category, 'image_name': image_name})
 
-            # Скачиваем полную версию изображения
-            yield scrapy.Request(response.urljoin(image_url), self.save_image)
-
-    def save_image(self, response):
-        # Создаем папку для категории, если она еще не существует
-        category = response.meta['category']
-        if not os.path.exists(f'images/{category}'):
-            os.makedirs(f'images/{category}')
-        image_url=response.xpath('//*[@class="WxXog"]/img/@srcset').extract()
-       
-        # Сохраняем изображение в папку категории
-        with open(f'images/{category}/{image_url}', 'wb') as f:
-            f.write(response.body)
+ def save_image(self, response):
+    category = response.meta['category']
+    image_name = response.meta['image_name']
+    if not os.path.exists(f'images/{category}'):
+        os.makedirs(f'images/{category}')
+    with open(f'images/{category}/{image_name}.jpg', 'wb') as f:
+        f.write(response.body)
